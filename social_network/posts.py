2d5afd44ab045cf6a2d8af01063617aae46ff033
maralegui <mara.leguizamon@gmail.com>
2019-04-16 14:53:37 +0000

    Passing all tests

--- social_network/posts.py
+++ social_network/posts.py
@@ -12,7 +12,7 @@ class Post(object):
 
 class TextPost(Post):  # Inherit properly
     def __init__(self, text, timestamp=None):
-        super().__init__(text, timestamp)
+        super(TextPost,self).__init__(text, timestamp)
         
     def __str__(self):
         return '@{} {}: "{}"\n\t{}'.format(self.user.first_name, self.user.last_name, self.text, self.timestamp)
@@ -20,7 +20,7 @@ class TextPost(Post):  # Inherit properly
 
 class PicturePost(Post):  # Inherit properly
     def __init__(self, text, image_url, timestamp=None):
-        super().__init__(text, timestamp)
+        super(PicturePost,self).__init__(text, timestamp)
         self.image_url = image_url
 
     def __str__(self):
@@ -29,7 +29,7 @@ class PicturePost(Post):  # Inherit properly
 
 class CheckInPost(Post):  # Inherit properly
     def __init__(self, text, latitude, longitude, timestamp=None):
-        super().__init__(text, timestamp)
+        super(CheckInPost,self).__init__(text, timestamp)
         self.latitude = latitude
         self.longitude = longitude
 
